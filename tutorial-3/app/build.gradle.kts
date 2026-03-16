2af8cbb94995a4f4092e33d9aef37eb2b9427c47
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2026-03-16 11:36:44 +1100

    tutorial 3 checkpoint

--- tutorial-3/app/build.gradle.kts
+++ tutorial-3/app/build.gradle.kts
@@ -10,6 +10,10 @@ android {
         }
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.myapplication"
         minSdk = 24
--- tutorial-3/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ tutorial-3/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,21 +1,47 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+const val USERNAME_KEY : String = "USERNAME"
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var ui : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        ui = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(ui.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(ui.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Log.d("MY_APP", "onCreate");
+
+        val myButton : Button = ui.btnEnter
+        myButton.setOnClickListener {
+            Log.d("MY_APP", "clicked! click me baby");
+            val enteredText = ui.txtName.text.toString()
+
+            val goToNextScreenIntent = Intent(this, SecondActivity::class.java)
+            goToNextScreenIntent.putExtra(USERNAME_KEY, enteredText)
+            startActivity(goToNextScreenIntent)
+        }
+
 
         //made a change to my project
         //made more changes
